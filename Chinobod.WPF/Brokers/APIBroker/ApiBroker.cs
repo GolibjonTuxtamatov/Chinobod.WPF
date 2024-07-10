2af19ffba44b4b2c1569042fa359080f5090cbd4
Golibjon <113083394+GolibjonTuxtamatov@users.noreply.github.com>
2024-07-10 10:14:20 +0500

    BROKERS: API Integrate -> WIP

---
+++ Chinobod.WPF/Brokers/APIBroker/ApiBroker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Chinobod.WPF.Models.News;
+
+namespace Chinobod.WPF.Brokers.APIBroker
+{
+    internal class ApiBroker : IApiBroker
+    {
+        private readonly HttpClient clien;
+
+        public ApiBroker()
+        {
+            this.clien = new HttpClient();
+        }
+
+        public ValueTask<News> InsertNewsAsync(News news)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<News> SelectAllNews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<News> SelectNewsByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<News> UpdateNewsAsync(News news)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<News> DeleteNewsAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chinobod.WPF/Brokers/APIBroker/IApiBroker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinobod.WPF.Models.News;
+
+namespace Chinobod.WPF.Brokers.APIBroker
+{
+    internal interface IApiBroker
+    {
+        ValueTask<News> InsertNewsAsync(News news);
+        IQueryable<News> SelectAllNews();
+        ValueTask<News> SelectNewsByIdAsync(Guid id);
+        ValueTask<News> UpdateNewsAsync(News news);
+        ValueTask<News> DeleteNewsAsync(Guid id);
+    }
+}

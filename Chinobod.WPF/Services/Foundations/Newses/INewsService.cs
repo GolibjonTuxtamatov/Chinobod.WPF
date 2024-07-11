a6aed593f0c5b34d91aca6143a7350e8f004168d
Golibjon <113083394+GolibjonTuxtamatov@users.noreply.github.com>
2024-07-11 15:04:57 +0500

    FOUNDATIONS: News Service Layer

---
+++ Chinobod.WPF/Services/Foundations/Newses/INewsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinobod.WPF.Models.News;
+
+namespace Chinobod.WPF.Services.Foundations.Newses
+{
+    public interface INewsService
+    {
+        ValueTask<News> AddNewsAsync(News news);
+        IQueryable<News> RetrieveAllNewses();
+        ValueTask<News> RetrieveNewsByIdAsync(Guid id);
+        ValueTask<News> ModifyNewsAsync(News news);
+        ValueTask<News> RemoveNewsAsync(Guid id);
+    }
+}
